181b9ae1947c0ffdbe94f178d03bee4f3a49be8d
Rashaad Akbar <rashaadzma@gmail.com>
2021-07-04 17:00:47 +0530

    Added support for ForeignKey's

--- pg_orm/models/__init__.py
+++ pg_orm/models/__init__.py
@@ -1,2 +1,8 @@
 from .base_model import create_model, create_async_model
 from .fields import *
+
+CASCADE = "CASCADE"
+NO_ACTION = "NO ACTION"
+RESTRICT = "RESTRICT"
+SET_NULL = "SET NULL"
+SET_DEFAULT = "SET DEFAULT"
