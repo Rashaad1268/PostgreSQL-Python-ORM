1c6d32deebc3a6d45790dd5a5fd26f652ec62031
Rashaad Akbar <rashaadzma@gmail.com>
2021-11-18 18:52:41 +0530

    Added init_db

--- pg_orm/__init__.py
+++ pg_orm/__init__.py
@@ -1,8 +1,21 @@
 import logging
 
-from pg_orm.models.base_model import create_model, Model
 from pg_orm.migrations.migration import (migrate, migrate_all,
                                          async_migrate, async_migrate_all)
+
+
+def init_db(psycopg2_pool=None, asyncpg_pool=None):
+    from pg_orm.models.base_model import Model, AsyncModel
+    from pg_orm.models.database import Psycopg2Driver, AsyncpgDriver
+
+    if not psycopg2_pool and not asyncpg_pool:
+        raise Exception("psycopg2_pool or asyncpg_pool must be specified.")
+
+    if psycopg2_pool:
+        Model.set_db(Psycopg2Driver(psycopg2_pool))
+
+    if asyncpg_pool:
+        AsyncModel.set_db(AsyncpgDriver(asyncpg_pool))
 
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
