4ebdcc0e99c01c3df15dedb0c7371298c50996aa
Rashaad Akbar <rashaadzma@gmail.com>
2021-11-18 18:53:05 +0530

    Added DataBaseNotConfigured

--- pg_orm/errors.py
+++ pg_orm/errors.py
@@ -17,6 +17,17 @@ class FiledError(DBError):
             f"Choices are: {', '.join(self.options)}"
         )
 
+
 class ValidationError(DBError):
     """Error raised when a data validator failed."""
     pass
+
+
+class DataBaseNotConfigured(DBError):
+    """Raised when the database is not properly configured"""
+    def __init__(self, function_name: str = None):
+        message = "DataBase is not properly configured."
+        if function_name:
+            message += f"\nPlease use {function_name} and configure the database properly."
+
+        super().__init__(message)
