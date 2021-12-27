3136ce4709b36b54537a94483d4faa0747a455bf
Rashaad <rashaadzma@gmail.com>
2021-12-27 11:07:15 +0530

    Changed imports

--- pg_orm/__init__.py
+++ pg_orm/__init__.py
@@ -3,18 +3,20 @@ import logging
 
 def init_db(*, psycopg2_pool=None, asyncpg_pool=None):
     from pg_orm.models.base_model import Model, AsyncModel
-    from pg_orm.models.database import Psycopg2Driver, AsyncpgDriver
+    from pg_orm.models.database import Psycopg2Wrapper, AsyncpgWrapper
 
     if not psycopg2_pool and not asyncpg_pool:
         raise Exception("psycopg2_pool or asyncpg_pool must be specified.")
 
     if psycopg2_pool:
-        Model.set_db(Psycopg2Driver(psycopg2_pool))
+        Model.set_db(Psycopg2Wrapper(psycopg2_pool))
 
     if asyncpg_pool:
-        AsyncModel.set_db(AsyncpgDriver(asyncpg_pool))
+        AsyncModel.set_db(AsyncpgWrapper(asyncpg_pool))
 
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __version__ = "0.5.5"
+
+del logging  # Delete it from the namespace
